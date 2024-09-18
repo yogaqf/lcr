0d42f2e604670b4fbafab1f31b6a026e0cdfdda6
yoga.qf <yoga.qf@gmail.com>
2024-09-18 14:13:21 +0700

    compress foto dan ubah font mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+
+"use client"
+import Image from 'next/image'
+import { useState } from 'react';
+import { Skeleton } from '@/components/ui/skeleton';
 import { Separator } from "@/components/ui/separator";
 import { Bebas_Neue } from "next/font/google";
 import { resortManager, accounting, frontOffice, salesMarketing, fbService, fbProduct, housekeeping, engineering, hrSecurity } from "./data.js"
@@ -13,12 +18,14 @@ const container = "sm:px-20 lg:px-30 xl:px-40 2xl:px-64 px-6 py-20"
 const wrapper = "mt-8 w-full grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 lg:gap-8  mb-10"
 const card = "aspect-square w-full bg-gray-200 rounded-xl flex flex-col justify-end bg-cover shadow-2xl overflow-hidden relative"
 const img = "absolute h-full w-full bg-cover transition-all duration-1000 ease-in-out hover:scale-110"
-const span = `${bebasNeue.className} font-bold text-[8vw] sm:text-[2.5vw] text-white leading-none`
+const span = `${bebasNeue.className} font-bold text-[20px] sm:text-[2.5vw] text-white leading-none`
 const separator = "relative bg-green-400 border-t-2 border-green-400 w-[12%]"
 
 
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(true)
+
   return (
     <div className={container}>
       {/* Resort Manager */}
@@ -26,7 +33,8 @@ const Home = () => {
       <Separator className=" bg-green-400 " />
       <div className={wrapper}>
         <div className={card}>
-          <div className={img} style={{ backgroundImage: `url(${resortManager[0].image})` }}></div>
+          {isLoading && <span className={span}>Loading...</span>}
+          <Image src={resortManager[0].image} width={300} height={300} alt={resortManager[0].firstName} className={img} onLoadingComplete={() => setIsLoading(false)} />
           <div className="w-full px-4">
             <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
           </div>
@@ -43,14 +51,30 @@ const Home = () => {
       <div className={wrapper}>
         {accounting.map((item, index) => (
           <div key={index} className={card}>
-            <div className={img} style={{ backgroundImage: `url(${item.image})` }}></div>
-            <div className="w-full px-4">
-              <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
-            </div>
-            <div className="z-40 flex flex-col pt-3 pb-4 px-4">
-              <span className={span}>{item.firstName}</span>
-              <span className={span}>{item.lastName}</span>
-            </div>
+            {isLoading && (
+              <>
+                <Skeleton className={img} />
+                <div className="w-full px-4">
+                  <Skeleton className="relative bg-slate-200 border-t-2 border-slate-200 w-[12%]" style={{ height: "0.29vw" }} />
+                  <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                  </div>
+                </div>
+              </>
+            )}
+            {!isLoading && (
+              <>
+                <Image src={item.image} width={300} height={300} alt={item.firstName} className={img} />
+                <div className="w-full px-4">
+                  <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
+                </div>
+                <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                  <span className={span}>{item.firstName}</span>
+                  <span className={span}>{item.lastName}</span>
+                </div>
+              </>
+            )}
           </div>
         ))}
       </div>
@@ -61,14 +85,30 @@ const Home = () => {
       <div className={wrapper}>
         {frontOffice.map((item, index) => (
           <div key={index} className={card}>
-            <div className={img} style={{ backgroundImage: `url(${item.image})` }}></div>
-            <div className="w-full px-4">
-              <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
-            </div>
-            <div className="z-40 flex flex-col pt-3 pb-4 px-4">
-              <span className={span}>{item.firstName}</span>
-              <span className={span}>{item.lastName}</span>
-            </div>
+            {isLoading && (
+              <>
+                <Skeleton className={img} />
+                <div className="w-full px-4">
+                  <Skeleton className="relative bg-slate-200 border-t-2 border-slate-200 w-[12%]" style={{ height: "0.29vw" }} />
+                  <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                  </div>
+                </div>
+              </>
+            )}
+            {!isLoading && (
+              <>
+                <Image src={item.image} width={300} height={300} alt={item.firstName} className={img} />
+                <div className="w-full px-4">
+                  <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
+                </div>
+                <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                  <span className={span}>{item.firstName}</span>
+                  <span className={span}>{item.lastName}</span>
+                </div>
+              </>
+            )}
           </div>
         ))}
       </div>
@@ -79,14 +119,30 @@ const Home = () => {
       <div className={wrapper}>
         {salesMarketing.map((item, index) => (
           <div key={index} className={card}>
-            <div className={img} style={{ backgroundImage: `url(${item.image})` }}></div>
-            <div className="w-full px-4">
-              <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
-            </div>
-            <div className="z-40 flex flex-col pt-3 pb-4 px-4">
-              <span className={span}>{item.firstName}</span>
-              <span className={span}>{item.lastName}</span>
-            </div>
+            {isLoading && (
+              <>
+                <Skeleton className={img} />
+                <div className="w-full px-4">
+                  <Skeleton className="relative bg-slate-200 border-t-2 border-slate-200 w-[12%]" style={{ height: "0.29vw" }} />
+                  <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                  </div>
+                </div>
+              </>
+            )}
+            {!isLoading && (
+              <>
+                <Image src={item.image} width={300} height={300} alt={item.firstName} className={img} />
+                <div className="w-full px-4">
+                  <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
+                </div>
+                <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                  <span className={span}>{item.firstName}</span>
+                  <span className={span}>{item.lastName}</span>
+                </div>
+              </>
+            )}
           </div>
         ))}
       </div>
@@ -97,14 +153,30 @@ const Home = () => {
       <div className={wrapper}>
         {fbService.map((item, index) => (
           <div key={index} className={card}>
-            <div className={img} style={{ backgroundImage: `url(${item.image})` }}></div>
-            <div className="w-full px-4">
-              <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
-            </div>
-            <div className="z-40 flex flex-col pt-3 pb-4 px-4">
-              <span className={span}>{item.firstName}</span>
-              <span className={span}>{item.lastName}</span>
-            </div>
+            {isLoading && (
+              <>
+                <Skeleton className={img} />
+                <div className="w-full px-4">
+                  <Skeleton className="relative bg-slate-200 border-t-2 border-slate-200 w-[12%]" style={{ height: "0.29vw" }} />
+                  <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                  </div>
+                </div>
+              </>
+            )}
+            {!isLoading && (
+              <>
+                <Image src={item.image} width={300} height={300} alt={item.firstName} className={img} />
+                <div className="w-full px-4">
+                  <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
+                </div>
+                <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                  <span className={span}>{item.firstName}</span>
+                  <span className={span}>{item.lastName}</span>
+                </div>
+              </>
+            )}
           </div>
         ))}
       </div>
@@ -116,14 +188,30 @@ const Home = () => {
       <div className={wrapper}>
         {fbProduct.map((item, index) => (
           <div key={index} className={card}>
-            <div className={img} style={{ backgroundImage: `url(${item.image})` }}></div>
-            <div className="w-full px-4">
-              <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
-            </div>
-            <div className="z-40 flex flex-col pt-3 pb-4 px-4">
-              <span className={span}>{item.firstName}</span>
-              <span className={span}>{item.lastName}</span>
-            </div>
+            {isLoading && (
+              <>
+                <Skeleton className={img} />
+                <div className="w-full px-4">
+                  <Skeleton className="relative bg-slate-200 border-t-2 border-slate-200 w-[12%]" style={{ height: "0.29vw" }} />
+                  <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                  </div>
+                </div>
+              </>
+            )}
+            {!isLoading && (
+              <>
+                <Image src={item.image} width={300} height={300} alt={item.firstName} className={img} />
+                <div className="w-full px-4">
+                  <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
+                </div>
+                <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                  <span className={span}>{item.firstName}</span>
+                  <span className={span}>{item.lastName}</span>
+                </div>
+              </>
+            )}
           </div>
         ))}
       </div>
@@ -134,32 +222,30 @@ const Home = () => {
       <div className={wrapper}>
         {housekeeping.map((item, index) => (
           <div key={index} className={card}>
-            <div className={img} style={{ backgroundImage: `url(${item.image})` }}></div>
-            <div className="w-full px-4">
-              <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
-            </div>
-            <div className="z-40 flex flex-col pt-3 pb-4 px-4">
-              <span className={span}>{item.firstName}</span>
-              <span className={span}>{item.lastName}</span>
-            </div>
-          </div>
-        ))}
-      </div>
-
-      {/* Engineering */}
-      <h1 className="font-bold text-2xl mb-2 text-slate-700">Engineering</h1>
-      <Separator className=" bg-green-400 " />
-      <div className={wrapper}>
-        {engineering.map((item, index) => (
-          <div key={index} className={card}>
-            <div className={img} style={{ backgroundImage: `url(${item.image})` }}></div>
-            <div className="w-full px-4">
-              <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
-            </div>
-            <div className="z-40 flex flex-col pt-3 pb-4 px-4">
-              <span className={span}>{item.firstName}</span>
-              <span className={span}>{item.lastName}</span>
-            </div>
+            {isLoading && (
+              <>
+                <Skeleton className={img} />
+                <div className="w-full px-4">
+                  <Skeleton className="relative bg-slate-200 border-t-2 border-slate-200 w-[12%]" style={{ height: "0.29vw" }} />
+                  <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                  </div>
+                </div>
+              </>
+            )}
+            {!isLoading && (
+              <>
+                <Image src={item.image} width={300} height={300} alt={item.firstName} className={img} />
+                <div className="w-full px-4">
+                  <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
+                </div>
+                <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                  <span className={span}>{item.firstName}</span>
+                  <span className={span}>{item.lastName}</span>
+                </div>
+              </>
+            )}
           </div>
         ))}
       </div>
@@ -170,14 +256,30 @@ const Home = () => {
       <div className={wrapper}>
         {hrSecurity.map((item, index) => (
           <div key={index} className={card}>
-            <div className={img} style={{ backgroundImage: `url(${item.image})` }}></div>
-            <div className="w-full px-4">
-              <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
-            </div>
-            <div className="z-40 flex flex-col pt-3 pb-4 px-4">
-              <span className={span}>{item.firstName}</span>
-              <span className={span}>{item.lastName}</span>
-            </div>
+            {isLoading && (
+              <>
+                <Skeleton className={img} />
+                <div className="w-full px-4">
+                  <Skeleton className="relative bg-slate-200 border-t-2 border-slate-200 w-[12%]" style={{ height: "0.29vw" }} />
+                  <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                    <Skeleton className="h-6 my-2 w-full rounded-md bg-slate-200" />
+                  </div>
+                </div>
+              </>
+            )}
+            {!isLoading && (
+              <>
+                <Image src={item.image} width={300} height={300} alt={item.firstName} className={img} />
+                <div className="w-full px-4">
+                  <Separator className={separator} style={{ borderTop: "0.29vw solid rgb(0, 255, 0)" }} />
+                </div>
+                <div className="z-40 flex flex-col pt-3 pb-4 px-4">
+                  <span className={span}>{item.firstName}</span>
+                  <span className={span}>{item.lastName}</span>
+                </div>
+              </>
+            )}
           </div>
         ))}
       </div>
@@ -187,7 +289,5 @@ const Home = () => {
     </div>
   );
 };
-
-
 
 export default Home;
